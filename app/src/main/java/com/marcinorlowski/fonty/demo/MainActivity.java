06c95beeacca1a8cac415c1537f96851d17720a9
Marcin Orlowski <mail@marcinorlowski.com>
2017-04-24 00:23:06 +0200

    Updated demo app

--- app/src/main/java/com/marcinorlowski/fonty/demo/MainActivity.java
+++ app/src/main/java/com/marcinorlowski/fonty/demo/MainActivity.java
@@ -13,9 +13,13 @@ package com.marcinorlowski.fonty.demo;
  *
  ******************************************************************************
  */
+
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -23,6 +27,8 @@ import android.widget.Toast;
 import com.marcinorlowski.fonty.Fonty;
 
 public class MainActivity extends AppCompatActivity {
+
+	protected Handler mHandler = new Handler();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +52,54 @@ public class MainActivity extends AppCompatActivity {
 
 		// apply our font replacement
 		Fonty.setFonts(this);
+
+		int[] ids = {R.id.github, R.id.button};
+		for (int id : ids) {
+			findViewById(id).setOnClickListener(mOnClickListener);
+		}
 	}
 
+
+	protected boolean mTil2FontyApplied = false;
+
+	protected View.OnClickListener mOnClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			switch (view.getId()) {
+				case R.id.button:
+					int[] ids = {R.id.til1, R.id.til2};
+					for (int id : ids) {
+						TextInputLayout til = (TextInputLayout)findViewById(id);
+						til.setErrorEnabled(true);
+						til.setError("Some error text");
+					}
+
+					mHandler.removeCallbacks(mRunnable);
+					mHandler.postDelayed(mRunnable, 3 * 1000);
+
+					break;
+
+				case R.id.github:
+					Intent intent = new Intent(Intent.ACTION_VIEW);
+					intent.setData(Uri.parse("https://github.com/MarcinOrlowski/Fonty"));
+					intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+					try {
+						startActivity(intent);
+					} catch (ActivityNotFoundException e) {
+						Toast.makeText(MainActivity.this, "Failed to launch Web Browser", Toast.LENGTH_SHORT).show();
+					}
+					break;
+			}
+		}
+	};
+
+	protected Runnable mRunnable = new Runnable() {
+		@Override
+		public void run() {
+			int[] ids = {R.id.til1, R.id.til2};
+			for (int id : ids) {
+				((TextInputLayout)findViewById(id)).setError(null);
+			}
+		}
+	};
 }
